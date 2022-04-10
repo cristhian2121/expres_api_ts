2c8a5655ee1c99e40459372ba6ad03a8ad8cd4a9
maestro_roshi <ccdelgadop@gmail.com>
2022-04-10 18:37:29 -0500

    config cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,22 @@ import express from "express"
 import { boomErrorHandler, errorHandler, logErrors } from "./middlewares/error.handler"
 import { routerApi } from "./routes"
 import helmet from "helmet"
+import cors, { CorsOptions } from "cors"
 
 const app = express()
 const port = 3200
 
 app.use(express.json())
+
+// config cors
+const whiteList = ["localhost:80"]
+const options: CorsOptions = {
+  origin: (origin, callback) => {
+    const isAllowed = whiteList.includes(origin as string)
+    isAllowed ? callback(null, true): callback(new Error())
+  }
+}
+app.use(cors(options))
 
 // avaliable routers
 routerApi(app)
